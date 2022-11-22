41c07f54e93aeadd6012dabb1af9d1838e853c3e
himaabastin <111218465+himaabastin@users.noreply.github.com>
2022-11-22 18:08:59 +0530

    Moved to mongo atlas

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://hima:mongodb123@watchhut.bxalcnj.mongodb.net/shopping?retryWrites=true&w=majority'
     const dbname='shopping'
 
 mongoClient.connect(url,(err,data)=>{
